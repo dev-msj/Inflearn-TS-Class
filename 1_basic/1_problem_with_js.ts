a9b4705062738dc48a4d25a7c02f660b0b06350e
dev-msj <msj944@naver.com>
2023-12-13 20:32:09 +0900

    섹션1 - TS가 나오게 된 배경

---
+++ 1_basic/1_problem_with_js.ts
@@ -0,0 +1,11 @@
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+console.log(add(1, 2)); // 3
+// console.log(add(1, '2')); // 컴파일 에러 발생
+
+/**
+ * TS를 사용하면 변수/함수/클래스 정의 등 타입을 명확하게 하여
+ * 작성된 코드가 원래 의도한 대로 사람들이 사용할 수 있도록 강제할 수 있다.
+ */
