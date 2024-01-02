076fe67d086a6ca9ab84ecd603093f868de24df6
dev-msj <msj944@naver.com>
2024-01-03 05:58:54 +0900

    주석 수정

--- 8_object/1_define_object.ts
+++ 8_object/1_define_object.ts
@@ -2,13 +2,13 @@
  * Object를 선언하는 기본적인 방법
  */
 
-// name, age property를 가진 object 선언
+// name, age property를 가진 key/value 형식의 object literal 선언 방식
 const codeFactory = {
   name: '코드 팩토리',
   age: 32,
 }
 
-// name, age property를 가진 interface 선언
+// name, age property를 가진 interface의 객체 선언 방식
 interface IPerson {
   name: string;
   age: number;
@@ -18,7 +18,7 @@ const iPerson: IPerson = {
   age: 30,
 }
 
-// name, age property를 가진 type 선언
+// name, age property를 가진 type의 객체 선언 방식
 type TPerson = {
   name: string;
   age: number;
