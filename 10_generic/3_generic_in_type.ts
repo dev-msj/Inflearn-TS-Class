77570332b90207f214cca2646e5abee8302741b3
dev-msj <msj944@naver.com>
2024-01-19 20:43:41 +0900

    누락된 설명 추가

--- 10_generic/3_generic_in_type.ts
+++ 10_generic/3_generic_in_type.ts
@@ -1,10 +1,12 @@
 /**
  * Generic in Type
+ * 
+ * type에 generic을 활용하여 유연한 타입 사용할 수 있다.
  */
 type GenericSimpleType<T> = T;
 
 const genericString: GenericSimpleType<string> = '코드팩토리';
-// const genericString2: GenericSimpleType = '코드팩토리';
+// const genericString2: GenericSimpleType = '코드팩토리'; // generic 타입 추론은 불가능하다.
 
 interface DoneState<T>{
     data: T[];
@@ -18,18 +20,22 @@ interface ErrorState {
     error: string;
 }
 
+// generic에 기본 타입을 지정해줄 수 있다.
 type State<T = number> = DoneState<T> | LoadingState | ErrorState;
 
 let state: State<string> = {
     data: ['123', '456'],
 }
 
+// LoadingState
 state = {
     requestedAt: new Date()
 }
 
+// ErrorState
 state = {error: 'error'};
 
+// 기본 타입이 잘 동작하는 것을 확인할 수 있다.
 let state2: State = {
     data: [123, 456]
 }
