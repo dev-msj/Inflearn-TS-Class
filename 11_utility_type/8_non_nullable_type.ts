18205fa2c03160bc9e469d6993ef7f2645ce0cf2
dev-msj <msj944@naver.com>
2024-01-25 20:11:49 +0900

    잘못 이해한 내용 삭제

--- 11_utility_type/8_non_nullable_type.ts
+++ 11_utility_type/8_non_nullable_type.ts
@@ -6,5 +6,3 @@
 
 // null과 undefined를 제외한 타입들로 구성된 타입이 생성된다.
 type NonNull = NonNullable<string | number | boolean | null | undefined | object>; // string | number | boolean | object
-
-// null이 반환될 가능성이 있는 상황에서 null을 제외하고 싶을 때 혹은 null이 없는 타입을 만들고 싶을 때 사용할 수 있을 것 같다.
