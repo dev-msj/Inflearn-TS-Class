848aa086e51df525ff0b90de48e649a744d5762b
dev-msj <msj944@naver.com>
2024-01-31 22:33:53 +0900

    설명 수정

--- 12_experimental_decorator/prototype_for_class_and_function.js
+++ 12_experimental_decorator/prototype_for_class_and_function.js
@@ -32,4 +32,4 @@ console.dir(CIdol.prototype, {showHidden: true}); // CIdol의 prototype에는 co
 console.dir(FIdol1.prototype, {showHidden: true}); // FIdol1의 prototype에는 constructor만 존재한다.
 console.dir(FIdol2.prototype, {showHidden: true}); // FIol2의 prototype에는 constructor와 dance가 존재한다.
 
-// 즉, 클래스로 정의된 객체의 메서드는 함수로 정의한 객체의 프로토타입에 메서드를 정의한 것과 같은 형태이다.
+// 즉, 클래스에 정의된 메서드는 함수의 프로토타입에 메서드를 정의한 것과 같은 형태이다.
